29caeab0494afcf8ecef9c7aad3c049cee6011ea
heejin1998 <bobae1998@naver.com>
2021-03-07 19:24:43 +0900

    3/7 알고리즘 공부(DFS/BFS)

---
+++ DFS_BFS/5-10.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 16:56:08 2021
+
+실전문제 3. 음료수 얼려 먹기
+"""
+graph = []
+icecream = 0
+
+N, M = map(int,input().split())
+for i in range(N):
+    graph.append(list(input()))
+    
+    
+print(graph)
+
