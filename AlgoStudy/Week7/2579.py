7deefc36a9483bb9e28441d2de99e3850776c2d8
heejin1998 <bobae1998@naver.com>
2021-12-04 01:46:38 +0900

    Update 2579.py - n==1일때 코너케이스

--- AlgoStudy/Week7/2579.py
+++ AlgoStudy/Week7/2579.py
@@ -5,12 +5,14 @@ dp = [0] * (n+1)
 stairs = [0]
 for i in range(n):
     stairs.append(int(input()))
+if n==1:
+    print(stairs[1])
+else:
+    dp[0] = 0
+    dp[1] = stairs[1]
+    dp[2] = stairs[1]+ stairs[2]
 
-dp[0] = 0
-dp[1] = stairs[1]
-dp[2] = stairs[1]+ stairs[2]
+    for i in range(3, n+1):
+        dp[i] = max(stairs[i]+ stairs[i-1]+ dp[i-3], stairs[i]+ dp[i-2])
 
-for i in range(3, n+1):
-    dp[i] = max(stairs[i]+ stairs[i-1]+ dp[i-3], stairs[i]+ dp[i-2])
-
-print(dp[n])
+    print(dp[n])
